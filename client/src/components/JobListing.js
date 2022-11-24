723c7ae83b7cd21da0c63aaf99b41a42e9a72545
Ivan <ivanrao@outlook.com>
2022-11-24 18:08:02 -0500

    changed job listing links to open the job listing page on indeed in a new tab

--- client/src/components/JobListing.js
+++ client/src/components/JobListing.js
@@ -4,7 +4,7 @@ export default function JobListing(props) {
 	const { company, img, title, link, location } = props.opportunity;
 	return (
 		<div className="job-listing">
-			<a href={link}>
+			<a href={link} target="_blank" rel="noreferrer">
 				<h2>{title.substr(0, title.indexOf("- job post"))}</h2>
 				<h4>{company}</h4>
 				<h4>{location}</h4>
